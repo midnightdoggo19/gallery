4e740211911066e84a92282c2b49e27761027a94
Midnight Doggo <admin@midnightdoggo19.com>
2024-12-03 22:08:42 +0000

    Updated for static hosting

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-fetch('/api/images')
+fetch('images.json')
   .then(response => response.json())
   .then(images => {
     const randomIndex = Math.floor(Math.random() * images.length);
     const randomImage = images[randomIndex];
 
     document.getElementById('background').style.backgroundImage = `url(${randomImage})`;
-    document.getElementById('caption').textContent = 'https://github.com/midnightdoggo19.com/gallery';
+    document.getElementById('caption').textContent = 'github.com/midnightdoggo19/gallery';
   })
   .catch(err => console.error('Error fetching images:', err));
